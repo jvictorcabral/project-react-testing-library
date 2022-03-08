02b181c0adc5329bbadf22f7680b494c123a2cc6
Joao <joaovictorcgjvcg@gmail.com>
2022-03-08 12:11:19 -0400

    Entrega testes FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const notFound = screen.getByText(/No favorite pokemon found/i);
+
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<App />);
+
+    const link = screen.getByRole('link', { name: /More details/ });
+    userEvent.click(link);
+
+    const favoriteCheck = screen.getByRole('checkbox', { name: /favoritado/ });
+    userEvent.click(favoriteCheck);
+
+    const favorite = screen.getByText('Favorite Pokémons');
+    userEvent.click(favorite);
+    const pikachu = screen.getByRole('img', { name: /Pikachu sprite/i });
+    expect(pikachu).toBeInTheDocument();
+  });
+});
